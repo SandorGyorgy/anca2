3ffc38a68b095016e16b26c1dfa0d06eb3b37400
Gyorgy Sandor <sandor1819@gmail.com>
2018-09-05 15:46:13 +0300

    fmm

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('remember_token');
+            $table->string('remember_token')->nullable();
             $table->boolean('is_admin')->default(0);
             $table->timestamps();
         });
